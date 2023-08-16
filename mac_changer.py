e86ca0d6ebe73046b8555a6dace6ae29f5ddc9fb
kimani nyutu <93473945+kimaninyutu@users.noreply.github.com>
2023-08-16 13:37:56 +0300

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,44 @@
-#!/usr/bin/ python
-
 import subprocess
+import optparse
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:11:22:33:44", shell=True)
-subprocess.call("ifconfig wlan0 up",shell=True)
+
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address")
+
+(options, arguments) = parser.parse_args()
+def Welcome():
+    print('''
+             AVAILABLE INTERFACES
+    -----------------------------------
+    -----------------------------------''')
+
+    subprocess.call(f"ip addr show", shell=True)
+
+def Mac_Changer():
+    interfaces = ['wlan0', 'eth0', 'l0', 'docker0']
+    interface = options.interface or input("Interface> ")
+    new_mac = options.new_mac or input("new MAC> ")
+    for x in interfaces:
+        if interface == x:
+            print(f"[+] Changing MAC address for {interface} to {new_mac}")
+            subprocess.call(["ifconfig", interface, "down"])
+            subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+            subprocess.call(["ifconfig", interface, "up"])
+            subprocess.call(f"ifconfig {interface} | grep ether", shell=True)
+            break
+    else:
+        print('!!Interface Not Found!!')
+
+
+
+Welcome()
+Mac_Changer()
+
+
+
+
+
+
 
 
